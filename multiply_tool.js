81bfff6fb83ccf3dd0f2f981a5039fb8627f5909
BellaTheAlien <161175405+BellaTheAlien@users.noreply.github.com>
2025-06-28 22:07:56 -0700

    making my first tool for the LLM to use, multiply tool

---
+++ multiply_tool.js
@@ -0,0 +1,38 @@
+import {tool} from "@langchain/core/tools";
+import {z} from "zod";
+import { HumanMessage } from "@langchain/core/messages";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-2.0-flash",
+  temperature: 0
+});
+
+const multiply = tool(
+    ({a, b}) => {
+       return a * b; 
+    },
+    {
+        name: "multiply",
+        description: "Multiply two numbers",
+        schema: z.object({
+            a: z.number(),
+            b: z.number(),
+        }),
+    }
+);
+
+//binding the tools to the LLM
+const llmWithTools = llm.bindTools([multiply]);
+
+console.log("=== Example 1: Basic Tool Usage ===");
+const response1 = await llmWithTools.invoke([
+  new HumanMessage("What is 15 multiplied by 23?")
+]);
+await multiply.invoke({ a: 2, b: 3});
+
+console.log("AI Response: ", response1.content);
+console.log(response1.description);
+console.log(multiply.name);
+console.log(multiply.description);
+//console.log("Tool calls: ", res;ponse1.tool_calls);
